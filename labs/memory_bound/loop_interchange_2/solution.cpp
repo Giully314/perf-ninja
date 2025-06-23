22af9dae6be28213715b8218a439348e66ab54b3
Giully314 <giully@protonmail.com>
2025-06-24 00:43:48 +0200

    solution loop interchange 2.

--- labs/memory_bound/loop_interchange_2/solution.cpp
+++ labs/memory_bound/loop_interchange_2/solution.cpp
@@ -10,38 +10,77 @@ static void filterVertically(uint8_t *output, const uint8_t *input,
                              const int *kernel, const int radius,
                              const int shift) {
   const int rounding = 1 << (shift - 1);
+  
 
-  for (int c = 0; c < width; c++) {
+  // We first identify the independent part of the nested loop and divide them.
+  // In this case it's pretty simple, they are already mentioned (top, middle and bottom).
+
+  for (int c = 0; c < width; c++) { 
     // Top part of line, partial kernel
     for (int r = 0; r < std::min(radius, height); r++) {
       // Accumulation
       int dot = 0;
       int sum = 0;
       auto p = &kernel[radius - r];
+      
       for (int y = 0; y <= std::min(r + radius, height - 1); y++) {
         int weight = *p++;
         dot += input[y * width + c] * weight;
         sum += weight;
       }
-
+      
       // Normalization
       int value = static_cast<int>(dot / static_cast<float>(sum) + 0.5f);
       output[r * width + c] = static_cast<uint8_t>(value);
     }
+  }
 
-    // Middle part of computations with full kernel
-    for (int r = radius; r < height - radius; r++) {
-      // Accumulation
-      int dot = 0;
-      for (int i = 0; i < radius + 1 + radius; i++) {
-        dot += input[(r - radius + i) * width + c] * kernel[i];
+  // Middle part of computations with full kernel  
+  
+  for (int r = radius; r < height - radius; r++) {
+    // auto dot = std::make_unique<int[]>((height - radius) * width);
+    int dot[width];
+    for (int c = 0; c < width; c++) { 
+      dot[c] = 0;
+    } 
+
+    for (int i = 0; i < radius + 1 + radius; i++) {
+        for (int c = 0; c < width; c++) { 
+          dot[c] += input[(r - radius + i) * width + c] * kernel[i];
       }
-
-      // Fast shift instead of division
-      int value = (dot + rounding) >> shift;
-      output[r * width + c] = static_cast<uint8_t>(value);
     }
 
+    for (int c = 0; c < width; c++) {
+    // Fast shift instead of division
+      int value = (dot[c] + rounding) >> shift;
+      output[r * width + c] = static_cast<uint8_t>(value);
+    }
+  }
+  
+  
+
+  // Old iteration.
+  // for (int c = 0; c < width; c++) {
+  //   // Middle part of computations with full kernel
+  //   for (int r = radius; r < height - radius; r++) {
+  //     // Part 1: initialization.
+  //     // Accumulation
+  //     int dot = 0;
+
+  //     // Part 2: accumulation.
+  //     for (int i = 0; i < radius + 1 + radius; i++) {
+  //       dot += input[(r - radius + i) * width + c] * kernel[i];
+  //       // intermediate[c] += input[(r - radius + i) * width + c] * kernel[i];
+  //     }
+
+  //     // Part 3: output.
+  //     // Fast shift instead of division
+  //     int value = (dot + rounding) >> shift;
+  //     output[r * width + c] = static_cast<uint8_t>(value);
+  //   }
+  // }
+
+  for (int c = 0; c < width; c++) {
     // Bottom part of line, partial kernel
     for (int r = std::max(radius, height - radius); r < height; r++) {
       // Accumulation
--- solutions/chapter9.md
+++ solutions/chapter9.md
@@ -14,3 +14,19 @@ We can also use a cache oblivious algorithm as described [here](https://en.wikip
 ## Loop interchange 1
 Sequantial memory access by swapping nested loops allows to use the whole cache and 
 to prefetch data from RAM more easily.  
+
+## Loop interchange 2
+The solution is basically given in the introduction video. We can call the 
+technique used "lift and memoize". The idea is to lift the accumulation variable
+into the upper loop and make it an array. Then we can apply the init, accumulate and save
+steps by dividing them into independent loop. We can repeat these step for more nested 
+loop and finally we can switch the order of the loops.
+
+
+
+# Exercise 2
+Use the compiler optimization report to check if vectorization were performed or not (and why). Also tools like Intel VTune reports if the vectorization it's being used or not. 
+
+# Exercise 4
+Function inlining. This will allow to remove the overhead of function prologue/epilogue
+and enhance optimization opportunities for the compiler.  
